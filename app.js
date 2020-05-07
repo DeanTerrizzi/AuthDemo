3f2a80dd3b20731421c22ef05fa37bf23c50467f
Dean Terrizzi <dterrizzi@outlook.com>
2020-05-07 18:59:40 -0400

    added register routes, added register form

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app", {
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
     resave: false,
@@ -24,12 +25,38 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//==============
+// ROUTES
+//=============
+
 app.get("/", function(req, res){
     res.render("home");
 });
 
 app.get("/secret", function(req, res){
     res.render("secret");
+});
+
+//  Auth Routes
+
+// show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handling user sign up
+app.post("/register", function(req, res){
+    // req.body.username
+    // req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
 });
 
 app.listen(3000, function(){
