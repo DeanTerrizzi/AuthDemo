e1b1077cc1f499f94ba7a9e1f5cf8ac0644c6129
Dean Terrizzi <dterrizzi@outlook.com>
2020-05-07 22:07:59 -0400

    Added Login routes and form

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -57,6 +58,19 @@ app.post("/register", function(req, res){
             res.redirect("/secret");
         });
     });
+});
+
+// LOGIN ROUTES
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// login logic
+// middleware
+app.post("/login",passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
 });
 
 app.listen(3000, function(){
